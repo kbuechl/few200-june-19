d9e79cd9e199bb460346c2cc779cb1f81fcdf1d2
Karl Buechler <karlrbuechler@gmail.com>
2019-06-10 18:45:10 +0000

    adding initial files from first few hours of class

---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { TodoListItem } from './models';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: `./todo-list.component.html`,
+  styleUrls: ['todo-list.component.css']
+})
+
+export class TodoListComponent {
+  items: TodoListItem[] =  [
+    { description: 'Mow Grass', completed: false},
+    { description:'Take Trash Out', completed: true}
+  ];
+
+  markComplete(item:TodoListItem){
+    item.completed = true
+  }
+  
+add(what: HTMLInputElement){
+  const description = what.value;
+ this.items.unshift({description, completed:false });
+
+  what.value='';
+  what.focus();
+}
+
+removeCompleted(){
+  this.items = this.items.filter(item=> !item.completed)
+}
+
+}
